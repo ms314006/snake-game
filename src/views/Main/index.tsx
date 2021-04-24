ee5b8bd6b93cd286b26b5d178d877ace3670fe25
GQSM <ms314006@hotmail.com>
2021-04-25 00:54:57 +0800

    1. add: 建立基本的版面配置 2. add: 完成基本的貪食蛇功能   - 按下上下左右會跑，且更新蛇身的每個部分的位置   - 吃到食物會變長，食物會消失然後重生在隨機一個地方   - 碰到自己的身體會停止遊戲   - 可透過簡單的可透過簡單的 restart 重新開始遊戲

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,15 +1,165 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const Title = styled.div`
-  font-size: 32px;
-  color: #0f4c75;
+const MAP_ROW_AND_COLUMN_SIZE = 13;
+
+const SnakeGame = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #1e212d;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'Raleway', sans-serif;
 `;
 
-const Main = () => (
-  <Title>
-    Hello world!
-  </Title>
-);
+const GameTitle = styled.div`
+  color: #faf3e0;
+  font-size: 52px;
+  margin: 60px 0px;
+`;
+
+const GameScreen = styled.div`
+  width: 520px;
+  height: 520px;
+  border: 2px solid #b68973;
+  border-radius: 4px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const MapGrid = styled.div`
+  width: 40px;
+  height: 40px;
+  border: 1px solid #b68973;
+  box-sizing: border-box;
+`;
+
+const GameCanvas = styled.canvas`
+  position: absolute;
+`;
+
+const Main = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const initialMoveDirection = null;
+  const initialSnakeLength = 5;
+
+  let [xv, yv] = [0, 0];
+  let [snakeHeadX, snakeHeadY] = [240, 240];
+  let [appleX, appleY] = [40, 40];
+  let currentMoveDirection = initialMoveDirection;
+  let isStartGame = false;
+  let snakeLength = initialSnakeLength;
+  let snakeBodys = [];
+
+  const draw = (drawIntervalId) => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext('2d');
+      if (!ctx) return;
+      ctx.clearRect(0, 0, 520, 520);
+
+      snakeHeadX += xv;
+      snakeHeadY += yv;
+
+      if (snakeHeadX < 0) {
+        snakeHeadX = 480;
+      }
+      if (snakeHeadX > 519) {
+        snakeHeadX = 0;
+      }
+      if (snakeHeadY < 0) {
+        snakeHeadY = 480;
+      }
+      if (snakeHeadY > 519) {
+        snakeHeadY = 0;
+      }
+      const isSnakeHeadTouchBody = (snakeBodyX, snakeBodyY) => (
+        snakeBodyX === snakeHeadX && snakeBodyY === snakeHeadY
+      );
+      ctx.fillStyle = '#eabf9f';
+      for (let i = 0; i < snakeBodys.length; i += 1) {
+        ctx.fillRect(snakeBodys[i].x + 1, snakeBodys[i].y + 1, 38, 38);
+        if (isStartGame && isSnakeHeadTouchBody(snakeBodys[i].x, snakeBodys[i].y)) {
+          snakeLength = initialSnakeLength;
+          clearInterval(drawIntervalId);
+        }
+      }
+      snakeBodys.push({ x: snakeHeadX, y: snakeHeadY });
+
+      if (snakeBodys.length > snakeLength) {
+        snakeBodys.shift();
+      }
+
+      if (appleX === snakeHeadX && appleY === snakeHeadY) {
+        snakeLength += 1;
+        [appleX, appleY] = [Math.floor(Math.random() * 13) * 40, Math.floor(Math.random() * 13) * 40];
+      }
+
+      ctx.fillStyle = '#faf3e0';
+      ctx.fillRect(appleX, appleY, 40, 40);
+    }
+  };
+
+  const initialGame = () => {
+    [xv, yv] = [0, 0];
+    [snakeHeadX, snakeHeadY] = [240, 240];
+    [appleX, appleY] = [40, 40];
+    currentMoveDirection = initialMoveDirection;
+    snakeLength = initialSnakeLength;
+    snakeBodys = [];
+    isStartGame = false;
+    const drawIntervalId = window.setInterval(() => draw(drawIntervalId), 150);
+  };
+
+  useEffect(() => {
+    const moveDirection = ({ keyCode }) => {
+      isStartGame = true;
+      const [TOP, RIGHT, BOTTOM, LEFT] = [38, 39, 40, 37];
+      switch (keyCode) {
+        case TOP:
+          if (currentMoveDirection === BOTTOM) return;
+          [xv, yv] = [0, -40];
+          break;
+        case RIGHT:
+          if (currentMoveDirection === LEFT) return;
+          [xv, yv] = [40, 0];
+          break;
+        case BOTTOM:
+          if (currentMoveDirection === TOP) return;
+          [xv, yv] = [0, 40];
+          break;
+        case LEFT:
+          if (currentMoveDirection === RIGHT) return;
+          [xv, yv] = [-40, 0];
+          break;
+        default:
+      }
+      currentMoveDirection = keyCode;
+    };
+    window.addEventListener('keydown', moveDirection);
+    initialGame();
+  }, []);
+
+  return (
+    <SnakeGame>
+      <GameTitle>
+        Snake Game
+        <span role="img" aria-label="snake">🐍</span>
+      </GameTitle>
+      <GameScreen>
+        <GameCanvas ref={canvasRef} width="520px" height="520px" />
+        {
+          Array.from(Array((MAP_ROW_AND_COLUMN_SIZE ** 2) - 1))
+            .map((number, index) => <MapGrid key={index} />)
+        }
+        <MapGrid />
+      </GameScreen>
+      <button type="button" onClick={initialGame}>
+        restart
+      </button>
+    </SnakeGame>
+  );
+};
 
 export default Main;
